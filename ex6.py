b2d5543d527ddb32a2042a176b52de507887e5c4
annkon22 <43612983+annkon22@users.noreply.github.com>
2018-10-26 22:18:34 +0200

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,3 +1,9 @@
+#In the hash table map implementation, the hash table size was chosen to be 101. 
+# If the table gets full, this needs to be increased. Re-implement the put method so that the table 
+# will automatically resize itself when the loading factor reaches a predetermined value 
+# (you can decide the value based on your assessment of load versus performance).
+
+
 class HashTable:
     def __init__(self):
         self.size = 3
