1269be8ba634fc352c4e739ca116845a84bab7e0
brandon <brandon_segura@outlook.com>
2018-10-27 20:25:26 +0300

    Update ex13.py
    Fixed your merge sort...

--- ex13.py
+++ ex13.py
@@ -4,42 +4,37 @@ import random
 def merge_sort(a_list):
     if len(a_list) <= 1:
         return a_list
-    else:
-        middle = len(a_list) // 2
-        left = [i for i in range(0, middle - 1)]
-        right = [i for i in range(middle, len(a_list))]
 
-        left = merge_sort(left)
-        right = merge_sort(right)
+    left = []
+    right = []
+    for i in range(len(a_list)):
+        if i < len(a_list) // 2:
+            left.append(a_list[i])
+        else:
+            right.append(a_list[i])
 
-        if left[len(left)] <= right[0]:
-            left.append(right)
-            return left
-        result = _merge(left, right)
-        return(result)
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return _merge(left, right)
 
 def _merge(left, right):
     result = []
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
-            result.append(left[0])
-            left[0].remove()
+            result.append(left.pop(0))            
         else:
-            result.append(right[0])
-            right[0].remove()
-    if len(left) > 0:
-        result.append(left)
-    if len(right) > 0:
-        result.append(right)
+            result.append(right.pop(0))            
+
+    while len(left) > 0:
+        result.append(left.pop(0))
+    while len(right) > 0:
+        result.append(right.pop(0))
     return result
 
 
 my_list = []
 for i in range(10):
     my_list.append(random.randrange(0, 100))
-
-merge_sort(my_list)
 print(my_list)
- 
-
-
+print(merge_sort(my_list))
