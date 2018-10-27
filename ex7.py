71df9d8a27aaa8fc51ea7d90ad22a01845521964
annkon22 <43612983+annkon22@users.noreply.github.com>
2018-10-27 03:12:51 +0200

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,77 @@
+#Implement quadratic probing as a rehash technique
+
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+
+    def put(self, key, data):
+        hash_value = self.hash_function(key, len(self.slots))
+
+        if self.slots[hash_value] == None:
+            self.slots[hash_value] = key
+            self.data[hash_value] = data
+        else:
+            if self.slots[hash_value] == key:
+                self.data[hash_value] == data   #replace
+
+            else:
+                next_slot = self.rehash(hash_value, len(self.slots))
+                while self.slots[next_slot] != None and self.slots[next_slot] != key:
+                    next_slot = self.rehash(next_slot, len(self.slots))
+
+                if self.slots[next_slot] == None:
+                    self.slots[next_slot] = key
+                    self.data[next_slot] = data
+                else:
+                    self.data[next_slot] = data     #replace
+
+    def hash_function(self, key, size):
+        return key % size
+
+    def rehash(self, old_hash, size):
+        for i in range(len(self.slots))
+            while i != len(self.slots)
+            quadr = pow(i, 2)
+            new_hash = (old_hash + quadr) % size
+            return new_hash
+
+    def get(self, key):
+        start_slot = self.hash_function(key, len(self.slots))
+
+        data = None
+        stop = False
+        found = False
+        position = start_slot
+        while self.slots[position] != None and not found and not stop:
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+            else:
+                position = self.rehash(position, len(self.slots))
+                if position == start_slot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+    
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+h = HashTable()
+h[54] = 'cat'
+h[26] = 'dog'
+h[93] = 'lion'
+h[17] = 'tiger'
+h[77] = 'bird'
+h[31] = 'cow'
+h[44] = 'goat'
+h[55] = 'pig'
+h[20] = 'chicken'
+print(h.slots)
+print(h.data)
+print()
